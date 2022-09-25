82870c0c1e646f78c515fe474de378fdc91faeaa
DHRUV SHRIVASTAVA <dhruvshrivastava18@gmail.com>
2022-09-25 18:17:20 +0530

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,18 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Dhruv Shrivastava on 28/08/22.
+//
+
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
